602dd4cd6b50434ca6960bd6450ea83f942f547a
Richard <richardd1894@gmail.com>
2025-09-09 22:05:13 -0400

    wip lifetime

--- src/common/lifetime.rs
+++ src/common/lifetime.rs
@@ -1,14 +1,18 @@
 use bevy::prelude::*;
 
+use crate::game::prelude::GameState;
+
 #[derive(Component)]
-pub struct Lifetime {
-    timer: Timer,
+pub enum Lifetime {
+    Timer(Timer),
+    State(GameState),
+    Entity(Entity),
 }
 
 impl Lifetime {
     pub fn from_duration(duration: f32) -> Self {
-        Lifetime {
-            timer: Timer::from_seconds(duration, TimerMode::Once),
-        }
+        Lifetime::Timer(Timer::from_seconds(duration, TimerMode::Once))
     }
+
+    pub fn finished(&self) {}
 }
