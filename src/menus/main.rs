c89c8b30935ef4aa19cd785627fffb048b66b94f
Richard Demers <richardd1894@gmail.com>
2026-07-22 18:59:44 -0400

    Play button start with Screen::Loading

--- src/menus/main.rs
+++ src/menus/main.rs
@@ -34,12 +34,11 @@ fn enter_loading_or_gameplay_screen(
     resource_handles: Res<ResourceHandles>,
     mut next_screen: ResMut<NextState<Screen>>,
 ) {
-    // if resource_handles.is_all_done() {
-    //     next_screen.set(Screen::Gameplay);
-    // } else {
-    //     next_screen.set(Screen::Loading);
-    // }
-    next_screen.set(Screen::Gameplay);
+    if resource_handles.is_all_done() {
+        next_screen.set(Screen::Gameplay);
+    } else {
+        next_screen.set(Screen::Loading);
+    }
 }
 
 fn open_settings_menu(_: On<Pointer<Click>>, mut next_menu: ResMut<NextState<Menu>>) {
