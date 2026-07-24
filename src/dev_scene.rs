afc08194df4849ce051b9c52ba866fcbae1860e8
Richard Demers <richardd1894@gmail.com>
2026-07-23 22:17:15 -0400

    test_planet bundle

--- src/dev_scene.rs
+++ src/dev_scene.rs
@@ -1,5 +1,8 @@
 use crate::{
-    game::playership::{PlayershipAssets, playership},
+    game::{
+        planet::test_planet,
+        playership::{PlayershipAssets, playership},
+    },
     screens::Screen,
 };
 use bevy::{color::palettes::css::*, prelude::*};
@@ -34,7 +37,8 @@ pub fn spawn_test_level(
                 MeshMaterial2d(materials.add(Color::from(GREEN))),
                 Transform::from_xyz(12., -10., 0.),
                 // Lifetime::from_secs(5.)
-            )
+            ),
+            test_planet(meshes, materials)
         ],
     ));
 }
--- src/game/planet.rs
+++ src/game/planet.rs
@@ -4,6 +4,7 @@ use avian2d::prelude::*;
 use bevy::{
     color::palettes::{css::GREEN, tailwind::BLUE_400},
     prelude::*,
+    ui_widgets::observe,
 };
 
 const ROTATE_SPEED: f32 = 1.0;
@@ -14,58 +15,56 @@ const SIZE_RADIUS: f32 = 10.;
 pub struct Planet;
 
 pub struct PlanetPlugin;
-
 impl Plugin for PlanetPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
-            FixedUpdate,
+            Update,
             // WARN: State should be cruising
             (rotate_planets).run_if(|| false),
         );
     }
 }
 
-pub fn spawn_test_planet(
-    mut commands: Commands,
+pub fn test_planet(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+) -> impl Bundle {
     // NOTE: velocity calc by distance from star?
 
-    commands
-        .spawn((
-            Name::new("Rock"),
-            Planet,
-            Collider::circle(SIZE_RADIUS),
-            Mesh2d(meshes.add(Circle::new(SIZE_RADIUS))),
-            MeshMaterial2d(materials.add(Color::from(BLUE_400))),
-            GravitySource::default(),
+    (
+        Name::new("Rock"),
+        Planet,
+        Collider::circle(SIZE_RADIUS),
+        CollisionEventsEnabled,
+        Mesh2d(meshes.add(Circle::new(SIZE_RADIUS))),
+        MeshMaterial2d(materials.add(Color::from(BLUE_400))),
+        GravitySource::default(),
+        RigidBody::Static,
+        Transform::from_xyz(100., 100., RadarZOrdering::Planet.z_order()),
+        observe(obs_planet_collision),
+        children![(
+            Station {
+                name: "Pebble".into(),
+            },
+            Collider::circle(2.),
+            Mesh2d(meshes.add(Circle::new(2.))),
+            MeshMaterial2d(materials.add(Color::from(GREEN))),
             RigidBody::Static,
-            Transform::from_xyz(100., 100., RadarZOrdering::Planet.z_order()),
-            children![(
-                Station {
-                    name: "Pebble".into(),
-                },
-                Collider::circle(2.),
-                Mesh2d(meshes.add(Circle::new(2.))),
-                MeshMaterial2d(materials.add(Color::from(GREEN))),
-                RigidBody::Static,
-                Transform::from_xyz(-50., 0., RadarZOrdering::Planet.z_order()),
-            )],
-        ))
-        .observe(obs_planet_collision);
+            Transform::from_xyz(-50., 0., RadarZOrdering::Planet.z_order()),
+        )],
+    )
 }
 
 fn obs_planet_collision(
-    trigger: On<CollisionStart>,
+    activate: On<CollisionStart>,
     planet_query: Query<&Name, (With<CollisionEventsEnabled>, With<Planet>)>,
     mut query: Query<&mut Health, With<CollisionEventsEnabled>>,
 ) {
-    if trigger.body1.is_none() {
+    if activate.body1.is_none() {
         return;
     }
-    let planet_entity = trigger.event_target();
-    let other_entity = trigger.collider1;
+    let planet_entity = activate.collider1;
+    let other_entity = activate.collider2;
 
     if let Ok(mut health) = query.get_mut(other_entity) {
         let name: String = match planet_query.get(planet_entity) {
